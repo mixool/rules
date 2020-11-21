02420f79844323b16b050fd85f4ff14d503fced1
Mcl <mixool0204@gmail.com>
2020-11-22 00:19:07 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export PATH
 ########
+# usage: bash <(curl -s https://raw.githubusercontent.com/mixool/shadowrocket-rules/main/generate.sh) category-porn
+#######
 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
