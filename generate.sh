8866cd4ea9693104683242c8760a44dda6b21a7e
Mcl <mixool0204@gmail.com>
2020-11-21 23:36:09 +0800

    GEOIP CN DIRECT

--- generate.sh
+++ generate.sh
@@ -31,6 +31,12 @@ ipv6 = false
 
 [Rule]
 $(domainlist cn | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,DIRECT/')
+IP-CIDR,192.168.0.0/16,DIRECT
+IP-CIDR,10.0.0.0/8,DIRECT
+IP-CIDR,192.168.0.0/16,DIRECT
+IP-CIDR,172.16.0.0/12,DIRECT
+IP-CIDR,127.0.0.0/8,DIRECT
+GEOIP,CN,DIRECT
 FINAL,PROXY
 
 [Host]
@@ -57,6 +63,12 @@ ipv6 = false
 [Rule]
 $(domainlist category-ads-all | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,Reject/')
 $(domainlist cn | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,DIRECT/')
+IP-CIDR,192.168.0.0/16,DIRECT
+IP-CIDR,10.0.0.0/8,DIRECT
+IP-CIDR,192.168.0.0/16,DIRECT
+IP-CIDR,172.16.0.0/12,DIRECT
+IP-CIDR,127.0.0.0/8,DIRECT
+GEOIP,CN,DIRECT
 FINAL,PROXY
 
 [Host]
