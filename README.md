be5c04f1238407bf1ca5b4c3fde6502c835b3ccc
Mcl <mixool0204@gmail.com>
2020-11-22 11:26:15 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-> 每日自动更新shadowrocket配置文件,由脚本[generate.sh](./generate.sh)使用[v2fly/domain-list-community](https://github.com/v2fly/domain-list-community/tree/master/data)列表生成  
+> 每日自动更新shadowrocket配置文件,由脚本[generate.sh](./generate.sh)使用[Loyalsoldier/v2ray-rules-dat](https://github.com/Loyalsoldier/v2ray-rules-dat)列表生成  
 > 屏蔽广告效果有限  
 
-黑名单: https://raw.githubusercontent.com/mixool/shadowrocket-rules/main/gfwlist.conf  
-黑名单+屏蔽广告: https://raw.githubusercontent.com/mixool/shadowrocket-rules/main/gfwlistplus.conf  
-  
-白名单:https://raw.githubusercontent.com/mixool/shadowrocket-rules/main/whitelist.conf  
-白名单+屏蔽广告:https://raw.githubusercontent.com/mixool/shadowrocket-rules/main/whitelistplus.conf  
+配置文件功能: 屏蔽广告,大陆可访问直连(包含Apple和Google可在大陆访问的域名),不可访问走代理,未匹配走代理
+配置文件地址: https://raw.githubusercontent.com/mixool/shadowrocket-rules/main/allrocket.conf
+
--- generate.sh
+++ generate.sh
@@ -7,8 +7,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
 
-# 生成指定域名列表 | 暂时无法使用regexp匹配
 function domainlist(){
+    # 从v2fly/domain-list-community提取指定类别域名列表
     wget -qO- "https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/$1" | grep -oE "^[a-zA-Z0-9./-].*" | sed -e "s/#.*//g" -e "s/@.*//g" >$TMPFILE
     includelistcn=$(cat $TMPFILE | grep -oE "include:.*" | cut -f2 -d: | tr "\n" " ") && sed -i -e "s/^include:.*//g" -e "s/^regexp:.*//g" -e "s/^full://g" -e "s/#.*//g" -e "s/@.*//g" $TMPFILE
     while [[ "$includelistcn" != "" ]]; do
@@ -20,8 +20,8 @@ function domainlist(){
     cat $TMPFILE | sort -u | sed "s/[[:space:]]//g" |sed "/^$/d"
 }
 
-# 生成白名单配置文件 cn
-function whitelistconf(){
+function allrocket(){
+    # 屏蔽广告,大陆可访问直连(包含Apple和Google可在大陆访问的域名),不可访问走代理,未匹配走代理
     cat <<EOF >$TMPFILE
 # Shadowrocket: $(date)
 [General]
@@ -32,69 +32,27 @@ dns-server = system
 ipv6 = false
 
 [Rule]
-$(domainlist cn | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,DIRECT/')
+# reject-list category-ads-all
+$(wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,Reject/' | sed "s/DOMAIN-SUFFIX,regexp/URL-REGEX/")
+
+# direct-list cn
+$(wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt | sed "s/^/DOMAIN-SUFFIX,&/" | sed "s/$/&,DIRECT/" | sed "s/DOMAIN-SUFFIX,regexp/URL-REGEX/")
+
+# direct-list apple 
+$(wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt | sed "s/^/DOMAIN-SUFFIX,&/" | sed "s/$/&,DIRECT/" | sed "s/DOMAIN-SUFFIX,regexp/URL-REGEX/")
+
+# direct-list google 
+$(wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt | sed "s/^/DOMAIN-SUFFIX,&/" | sed "s/$/&,DIRECT/" | sed "s/DOMAIN-SUFFIX,regexp/URL-REGEX/")
+
+# proxy-list 
+$(wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/greatfire.txt  | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,PROXY/' | sed "s/DOMAIN-SUFFIX,regexp/URL-REGEX/")
+$(wget -qO- https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt  | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,PROXY/' | sed "s/DOMAIN-SUFFIX,regexp/URL-REGEX/")
+
+# IP-CIDR
 IP-CIDR,192.168.0.0/16,DIRECT
 IP-CIDR,10.0.0.0/8,DIRECT
 IP-CIDR,172.16.0.0/12,DIRECT
 IP-CIDR,127.0.0.0/8,DIRECT
-GEOIP,CN,DIRECT
-FINAL,PROXY
-
-[Host]
-localhost = 127.0.0.1
-[URL Rewrite]
-^http://(www.)?g.cn https://www.google.com 302
-^http://(www.)?google.cn https://www.google.com 302
-EOF
-
-cat $TMPFILE
-}
-
-# 生成白名单+屏蔽广告配置文件
-function whitelistplusconf(){
-    cat <<EOF >$TMPFILE
-# Shadowrocket: $(date)
-[General]
-bypass-system = true
-skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, captive.apple.com
-bypass-tun = 10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,224.0.0.0/4,255.255.255.255/32
-dns-server = system
-ipv6 = false
-
-[Rule]
-$(domainlist category-ads-all | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,Reject/')
-$(domainlist cn | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,DIRECT/')
-IP-CIDR,192.168.0.0/16,DIRECT
-IP-CIDR,10.0.0.0/8,DIRECT
-IP-CIDR,172.16.0.0/12,DIRECT
-IP-CIDR,127.0.0.0/8,DIRECT
-GEOIP,CN,DIRECT
-FINAL,PROXY
-
-[Host]
-localhost = 127.0.0.1
-[URL Rewrite]
-^http://(www.)?g.cn https://www.google.com 302
-^http://(www.)?google.cn https://www.google.com 302
-EOF
-
-cat $TMPFILE
-}
-
-# 生成黑名单配置文件 geolocation-!cn by gfwlist
-function gfwlistconf(){
-    gfwlist_url="https://raw.githubusercontent.com/v2fly/domain-list-community/release/gfwlist.txt"
-    cat <<EOF >$TMPFILE
-# Shadowrocket: $(date)
-[General]
-bypass-system = true
-skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, captive.apple.com
-bypass-tun = 10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,224.0.0.0/4,255.255.255.255/32
-dns-server = system
-ipv6 = false
-
-[Rule]
-$(wget -qO- $gfwlist_url | base64 -d | grep -oE "^\|\|.*" | sed "s/||/DOMAIN-SUFFIX,/" | sed 's/$/&,PROXY/')
 IP-CIDR,91.108.4.0/22,PROXY,no-resolve
 IP-CIDR,91.108.8.0/22,PROXY,no-resolve
 IP-CIDR,91.108.12.0/22,PROXY,no-resolve
@@ -105,47 +63,14 @@ IP-CIDR,149.154.160.0/20,PROXY,no-resolve
 IP-CIDR,2001:b28:f23d::/48,PROXY,no-resolve
 IP-CIDR,2001:b28:f23f::/48,PROXY,no-resolve
 IP-CIDR,2001:67c:4e8::/48,PROXY,no-resolve
-FINAL,DIRECT
+
+# FINAL
+GEOIP,CN,DIRECT
+FINAL,PROXY
 
 [Host]
 localhost = 127.0.0.1
-[URL Rewrite]
-^http://(www.)?g.cn https://www.google.com 302
-^http://(www.)?google.cn https://www.google.com 302
-EOF
 
-cat $TMPFILE
-}
-
-# 生成黑名单+屏蔽广告配置文件
-function gfwlistplusconf(){
-    gfwlist_url="https://raw.githubusercontent.com/v2fly/domain-list-community/release/gfwlist.txt"
-    cat <<EOF >$TMPFILE
-# Shadowrocket: $(date)
-[General]
-bypass-system = true
-skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, captive.apple.com
-bypass-tun = 10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,224.0.0.0/4,255.255.255.255/32
-dns-server = system
-ipv6 = false
-
-[Rule]
-$(domainlist category-ads-all | sed "s/^/DOMAIN-SUFFIX,&/" | sed 's/$/&,Reject/')
-$(wget -qO- $gfwlist_url | base64 -d | grep -oE "^\|\|.*" | sed "s/||/DOMAIN-SUFFIX,/" | sed 's/$/&,PROXY/')
-IP-CIDR,91.108.4.0/22,PROXY,no-resolve
-IP-CIDR,91.108.8.0/22,PROXY,no-resolve
-IP-CIDR,91.108.12.0/22,PROXY,no-resolve
-IP-CIDR,91.108.16.0/22,PROXY,no-resolve
-IP-CIDR,91.108.56.0/22,PROXY,no-resolve
-IP-CIDR,109.239.140.0/24,PROXY,PROXY
-IP-CIDR,149.154.160.0/20,PROXY,no-resolve
-IP-CIDR,2001:b28:f23d::/48,PROXY,no-resolve
-IP-CIDR,2001:b28:f23f::/48,PROXY,no-resolve
-IP-CIDR,2001:67c:4e8::/48,PROXY,no-resolve
-FINAL,DIRECT
-
-[Host]
-localhost = 127.0.0.1
 [URL Rewrite]
 ^http://(www.)?g.cn https://www.google.com 302
 ^http://(www.)?google.cn https://www.google.com 302
@@ -155,17 +80,8 @@ cat $TMPFILE
 }
 
 case $1 in
-    whitelistconf)
-        whitelistconf
-        ;;
-    whitelistplusconf)
-        whitelistplusconf
-        ;;
-    gfwlistconf)
-        gfwlistconf
-        ;;
-    gfwlistplusconf)
-        gfwlistplusconf
+    allrocket)
+        allrocket
         ;;
     *)
         domainlist $1
