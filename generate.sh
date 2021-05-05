d8e8a0b4a91e84ee85a4eabfd89cb72871322c95
mixool <mixool0204@gmail.com>
2021-05-05 08:39:47 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export PATH
 #######
 # usage: bash <(curl -s https://raw.githubusercontent.com/mixool/rules/main/generate.sh) category-porn
-#######
+######
 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
